3f7a6ef11a60ae42a7caea2f970980fa8fabd3a2
Eyitope IO <adeyitope.io@gmail.com>
2022-06-23 22:21:22 +0100

    Sort data to pros, cons, date

--- AmazonReiview.py
+++ AmazonReiview.py
@@ -1,60 +1,57 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import requests
-
-
-# In[11]:
-
-
-import requests
+import pprint
 import json
+
 
 
 class MakeApiCall:
 
-    def get_data(self, api):
-        response = requests.get(f"{api}")
-        if response.status_code == 200:
-            print("sucessfully fetched the data")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
-
-    def get_user_data(self, api, parameters):
-        response = requests.get(f"{api}", params=parameters)
-        if response.status_code == 200:
-            print("sucessfully fetched the data with parameters provided")
-            self.formatted_print(response.json())
-        else:
-            print(
-                f"Hello person, there's a {response.status_code} error with your request")
-
-    def formatted_print(self, obj):
-        text = json.dumps(obj, sort_keys=True, indent=4)
-        print(text)
-
     def __init__(self, api):
-        # self.get_data(api)
+        self.main_list = []
+        self.reviews = ""
+        self.get_data(api)
 
-        parameters = {
-            "pros": " ",
-            "cons": " ",
-            "date": " "
-        }
-        self.get_user_data(api,parameters)
+    def get_data(self, api):
+        response = requests.get(api)
+        if response.status_code == 200:
+            print("Successfully fetched the data")
+            self.reviews = json.loads(response.content)
+        else:
+            print(f"Hello person, there's a {response.status_code} error with your request")
 
+    def sort_data(self):
+        self.reviews = self.reviews['data']['reviews']
+        for rev in self.reviews:
+
+            data_id = self.reviews["Id"]
+            
+            try:
+                likes = rev["LikesText"]
+            except KeyError:
+                likes = ""
+            try:
+                dislikes = rev["DisLikesText"]
+            except KeyError:
+                dislikes = ""
+            try:
+                created = rev["Created"]
+            except KeyError:
+                created = ""
+
+            self.main_list.append([likes, dislikes, created])
+
+    def show_all(self):
+        print("=== Sorted data ===") 
+        for i in self.main_list:
+            print(i)
 
 if __name__ == "__main__":
     api_call = MakeApiCall("https://www.ambitionbox.com/api/v2/reviews/data/114?page=1&sort=recent")
-
-
-# In[ ]:
-
+    api_call.sort_data()
+    api_call.show_all()
 
 
 
